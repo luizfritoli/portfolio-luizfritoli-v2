8a4427bfe4af0bafcca54c446a54c45f9bd5750c
luizfritoli <luizgustavofritoli@gmail.com>
2025-08-14 12:22:16 -0300

    SVG da navbar mobile alterada

--- src/components/NavbarMobile/index.jsx
+++ src/components/NavbarMobile/index.jsx
@@ -24,7 +24,7 @@ const NavbarMobile = () => {
               <span className="sr-only">Abrir menu</span>
               {!isOpen ? (
                 <svg
-                  className="h-6 w-6"
+                  className="h-6 w-6 text-purple-600"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -39,7 +39,7 @@ const NavbarMobile = () => {
                 </svg>
               ) : (
                 <svg
-                  className="h-6 w-6"
+                  className="h-6 w-6 text-purple-600"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
