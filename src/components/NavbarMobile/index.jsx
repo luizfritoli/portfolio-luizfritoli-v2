a4d35dd6edcaea78c5deeff382d58910989bf93b
luizfritoli <luizgustavofritoli@gmail.com>
2026-01-07 21:49:01 -0300

    Ajustes de cor na navbar mobile

--- src/components/NavbarMobile/index.jsx
+++ src/components/NavbarMobile/index.jsx
@@ -18,13 +18,13 @@ const NavbarMobile = () => {
           <div className="flex md:hidden">
             <button
               onClick={() => setIsOpen(!isOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md hover:text-blue-600 hover:bg-gray-100 focus:outline-none"
+              className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none"
               aria-expanded="false"
             >
               <span className="sr-only">Abrir menu</span>
               {!isOpen ? (
                 <svg
-                  className="h-6 w-6 text-purple-600"
+                  className="h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -39,7 +39,7 @@ const NavbarMobile = () => {
                 </svg>
               ) : (
                 <svg
-                  className="h-6 w-6 text-purple-600"
+                  className="h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -59,7 +59,7 @@ const NavbarMobile = () => {
       </div>
 
       <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-[#3b2066ba]">
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-[#CFCCCC] text-[#5E5E5E]">
           <a
             href="#greeting"
             className="block px-3 py-2 rounded-md text-base font-medium"
